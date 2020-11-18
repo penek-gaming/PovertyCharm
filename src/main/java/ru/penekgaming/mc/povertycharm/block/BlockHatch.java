20edb9fca5b2b37f8cd61f57819ea73b9120a297
Egor <thewgosplayer@gmail.com>
2020-11-18 18:21:10 +0300

    Fix collision bug when facing south

--- src/main/java/ru/penekgaming/mc/povertycharm/block/BlockHatch.java
+++ src/main/java/ru/penekgaming/mc/povertycharm/block/BlockHatch.java
@@ -43,8 +43,8 @@ public class BlockHatch extends PovertyBlockMeta<EnumFacing> {
     public static final AxisAlignedBBContainer BB_LADDER = AxisAlignedBBContainer.builder()
             .set(EnumFacing.NORTH, new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 1.0, 3 / 16.0))
             .set(EnumFacing.EAST, new AxisAlignedBB(1 - 3 / 16.0, 0.0, 0.0, 1.0, 1.0, 1.0))
-            .set(EnumFacing.WEST, new AxisAlignedBB(0.0, 0.0, 1 - 3 / 16.0, 1.0, 1.0, 1.0))
-            .set(EnumFacing.SOUTH, new AxisAlignedBB(0.0, 0.0, 0.0, 3 / 16.0, 1.0, 1.0))
+            .set(EnumFacing.SOUTH, new AxisAlignedBB(0.0, 0.0, 1 - 3 / 16.0, 1.0, 1.0, 1.0))
+            .set(EnumFacing.WEST, new AxisAlignedBB(0.0, 0.0, 0.0, 3 / 16.0, 1.0, 1.0))
             .build();
 
     public BlockHatch() {
@@ -105,14 +105,6 @@ public class BlockHatch extends PovertyBlockMeta<EnumFacing> {
         return iblockstate;
     }
 
-//    @Override
-//    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
-//        if(worldIn.isRemote)
-//            return;
-//
-//        worldIn.setBlockState(pos, ladderDown(state, worldIn, pos));
-//    }
-
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
         boolean upper = state.getValue(UPPER);
@@ -149,6 +141,7 @@ public class BlockHatch extends PovertyBlockMeta<EnumFacing> {
         return state.withProperty(LADDER, isLadderDown(state, world, pos));
     }
 
+    @SuppressWarnings("ConstantConditions")
     private boolean isLadderDown(IBlockState state, IBlockAccess world, BlockPos pos) {
         IBlockState downBS = world.getBlockState(pos.offset(EnumFacing.DOWN));
 
